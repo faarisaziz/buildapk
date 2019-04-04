92b7dd052a210e29d6ae806fa6238f7abfd14b6c
faarisaziz <faarisaziz@gmail.com>
2019-04-04 18:37:55 +0700

    update API

--- www/js/functions.js
+++ www/js/functions.js
@@ -1,6 +1,6 @@
-var urlLocation = "http://192.168.1.10:8000/";
-var mediaLocation = "http://192.168.1.10:8000/media/";
-var urlOdoo = "http://192.168.1.10:4000/";
+var urlLocation = "http://192.168.10.66:8000/";
+var mediaLocation = "http://192.168.10.66:8000/media/";
+var urlOdoo = "http://192.168.10.66:4000/";
 
 // http://192.168.10.66:4000/
 // http://mobeng.dcsys.id/
@@ -9,7 +9,8 @@ var verify = false;
 var phonenumber = '';
 var username = '';
 var email = '';
-var password = '';
+var pwd = '';
+var userid = '';
 
 get_banner_mobile();
 
@@ -29,7 +30,9 @@ $('#signup-check').click(function(e){
                 $('#signup-email').val(data.email);
                 email = data.email;
                 $('#signup-password').val(data.password);
+                pwd = data.password;
                 phonenumber = data.mobile;
+                userid = data.user_id;
                 verify = true;
                 $.mobile.toast({
                     message: 'Nomor tersedia'
@@ -61,7 +64,7 @@ $('#signup-verify').click(function(e){
             data: JSON.stringify({
                 "telp" : phonenumber
             }),
-            success: function() {
+            success: function(data) {
                 $.mobile.changePage("#verify");
                 $.mobile.toast({
                     message: 'kode OTP telah dikirim'
@@ -69,10 +72,8 @@ $('#signup-verify').click(function(e){
             },
             complete: function(data) {
                 add_user(data.fullname, data.email, data.apps_password, data.mobile);
-                console.log(data);
             },
             error: function( errorThrown ){
-                console.log('jajajangjajajajjang')
                 console.log(errorThrown.error);
             }
         });
@@ -84,6 +85,30 @@ $('#signup-verify').click(function(e){
     e.preventDefault();
 })
 
+function resend_auth(){
+    $.ajax({
+        type: 'post',
+        url: urlLocation + "api/authentication",
+        contentType: 'aplication/json',
+        dataType: 'json',
+        data: JSON.stringify({
+            "telp" : phonenumber
+        }),
+        success: function(data) {
+            $.mobile.changePage("#verify");
+            $.mobile.toast({
+                message: 'kode OTP telah dikirim ulang'
+            });
+        },
+        complete: function(data) {
+            add_user(data.fullname, data.email, data.apps_password, data.mobile);
+        },
+        error: function( errorThrown ){
+            console.log(errorThrown.error);
+        }
+    });
+}
+
 function add_user() {
     $.ajax({
         type: 'post',
@@ -93,11 +118,12 @@ function add_user() {
         data: JSON.stringify({
             "txt_fullname" : username,
             "email" : email,
-            "password" : password,
-            "telp" : phonenumber
+            "txt_password" : pwd,
+            "telp" : phonenumber,
+            "user_id": userid
         }),
         success: function(data) {
-            console.log(data);
+            console.log(pwd);
         },
         error: function( errorThrown ){
             console.log(errorThrown);
@@ -105,31 +131,44 @@ function add_user() {
     });
 }
 
-// $('#verify-next').click(function(e){
-//     if(verify){
-//         $.ajax({
-//             type: 'post',
-//             url: urlLocation + "api/add_user",
-//             contentType: 'aplication/json',
-//             dataType: 'json',
-//             data: JSON.stringify({
-//                 "telp" : phonenumber
+$('#verify-next').click(function(e){
+    $.ajax({
+        type: 'post',
+        url: urlLocation + "api/verify",
+        contentType: 'aplication/json',
+        dataType: 'json',
+        data: JSON.stringify({
+            "telp" : phonenumber,
+            "otp" : $('#1').val()+$('#a').val()+$('#b').val()+$('#c').val()+$('#d').val()+$('#e').val()
+        }),
+        success: function() {
+            $.mobile.changePage("#profile");
+        },
+        error: function( errorThrown ){
+            console.log(errorThrown);
+        }
+    });
+    e.preventDefault();
+})
 
-//             }),
-//             success: function() {
-//                 $.mobile.changePage("#profile");   
-//             },
-//             error: function( errorThrown ){
-//                 console.log(errorThrown);
-//             }
-//         });
-//     } else {
-//         $.mobile.toast({
-//             message: 'Lengkapi data terlebih dahulu'
-//         });
-//     }
-//     e.preventDefault();
-// })
+$('#profile-next').click(function(e){
+    $.ajax({
+        type: 'post',
+        url: urlOdoo + "get/telp",
+        contentType: 'aplication/json',
+        dataType: 'json',
+        data: JSON.stringify({
+            
+        }),
+        success: function() {
+            $.mobile.changePage("#dashboard");
+        },
+        error: function( errorThrown ){
+            console.log(errorThrown);
+        }
+    });
+    e.preventDefault();
+})
 
 function get_banner_mobile() {
     $.ajax({
@@ -200,6 +239,54 @@ function get_detail_cabang(id) {
     });
 }
 
+function get_all_produk() {
+    $.ajax({
+        type: 'get',
+        url: urlLocation + "api/produk",
+        contentType: 'aplication/json',
+        dataType: 'json',
+        success: function( data ) {
+            var html = "";
+
+            data.forEach(element => {
+                html += '<div class="container-cabang">';
+                html += '<a href="#cabang-detail" onclick="get_detail_cabang('+element.pk+')"><img src="'+mediaLocation+element.fields.img_path+'"width="100%" height="150px" class="cabang-img"></a>';
+                html += '<div class="text-cabang">'+element.fields.name+'</div>';
+                html += '</div>';
+            });
+
+            $('#all-produk').html(html);
+        },
+        error: function( errorThrown ){
+            console.log(errorThrown);
+        }
+    });
+}
+
+function get_detail_produk(id) {
+    $.ajax({
+        type: 'get',
+        url: urlLocation + "api/get/produk/"+id,
+        contentType: 'aplication/json',
+        dataType: 'json',
+        success: function( data ) {
+            var html = "";
+
+            html += '<iframe width="100%" height="200" frameborder="0" style="border:0" ';
+            html += 'src="https://www.google.com/maps/embed/v1/place?q='+data[0].lat+','+data[0].lang+'&amp;key=AIzaSyACMR6xDxS1kbYtcgN8IMGH_oRu1VF-6Po"></iframe>';
+            html += '<div role="main" class="ui-content" id="map-canvas"><div class="text-cabang-normal">'+data[0].address+'</div>';
+            html += '<div class="text-cabang-bold">Email    : '+data[0].email+'</div>';
+            html += '<div class="text-cabang-bold">Call     : '+data[0].Call+'</div>';
+            html += '<div class="text-cabang-bold">Phone    : '+data[0].Phone+'</div></div>';
+
+            $('#detail-produk').html(html);
+        },
+        error: function( errorThrown ){
+            console.log(errorThrown);
+        }
+    });
+}
+
 function get_all_promotion() {
     $.ajax({
         type: 'get',
@@ -246,7 +333,7 @@ function get_detail_promotion(id) {
     });
 }
 
-// $('#profile-type').click(function(e){
+// function get_vehicle() {
 //     $.ajax({
 //         type: 'post',
 //         url: urlOdoo + "api/estimated/vehicle",
@@ -258,45 +345,152 @@ function get_detail_promotion(id) {
 //         }),
 //         success: function( data ) {
 //             console.log(data);
-//             var html = "";
+//             $('#profile-type').autocomplete({
+//                 minLength: 0,
+//                 source: data.model,
+//                 select: function( event, ui ) {
+//                     $('#profile-type').val(ui.item,value);
+//                     return false;
+//                 }
+//             })
             
 //         },
 //         error: function( errorThrown ){
 //             console.log(errorThrown);
 //         }
 //     });
-//     e.preventDefault();
-// })
+// }
 
-var vehicle = {  
-    url: function(key_token, txt_vehicle) {
-        return urlOdoo + "api/estimated/vehicle";
-    },
+// $('#profile-type').autocomplete({
+//     source: function (request, response) {
+//         $.ajax({
+//             type: 'post',
+//             url: urlOdoo + "api/estimated/vehicle",
+//             data: {
+//                 key_token: "a53c6d8a114ebf02d0fb05782534c738bb8f1c8845",
+//                 txt_vehicle: $('#profile-type').val()
+//             },
+//             success: response,
+//             dataType: 'json',
+//             minLength: 2,
+//             delay: 100
+//         });
+//     }
+// });
 
-    getValue: function(element) {
-        console.log(model);
-        return model;
-    },
+// $("#profile-type").focusout(function() {
+//     if ($("#profile-type").val() === '') {
+//         $('#profile-type').val('');
+//     }
+// });
 
-    ajaxSettings: {
-        dataType: "json",
-        method: "POST",
-        data: {
-            dataType: "json"
+function get_all_reminder() {
+    $.ajax({
+        type: 'post',
+        url: urlOdoo + "api/notice/reminder/",
+        contentType: 'aplication/json',
+        dataType: 'json',
+        data: JSON.stringify({
+            "key_token" : "a53c6d8a114ebf02d0fb05782534c738bb8f1c8845",
+            "user_id" : user_id
+        }),
+        success: function( data ) {
+            var html = "";
+            
+            data.forEach(element => {
+                html += '<div class="card-riwayat">';
+                html += '<div class="item">Servis</div>';
+                html += '<div class="item">Tanggal</div>';
+                html += '<div class="item">Kendaraan</div>';
+                html += '<div class="item">KM</div>';
+                html += '<div class="item">Oli</div>';
+                html += '<div class="item">Waktu</div>';
+                html += '<div class="item">Notes</div>';
+                html += '<a href="#reminder-detail" class="ui-btn btn-riwayat ui-btn-inline ui-corner-all">Detail</a>';
+                html += '</div>';
+            });
+
+            $('#reminder-list').html(html);
+        },
+        error: function( errorThrown ){
+            console.log(errorThrown);
         }
-    },
+    });
+}
 
-    preparePostData: function(data) {
-        data.key_token = "a53c6d8a114ebf02d0fb05782534c738bb8f1c8845",
-        data.txt_vehicle = $("#profile-type").val();
-        return data;
-    },
+function get_detail_reminder(id) {
+    $.ajax({
+        type: 'get',
+        url: urlOdoo + "api/get/product/reminder"+id,
+        contentType: 'aplication/json',
+        dataType: 'json',
+        success: function( data ) {
+            var html = "";
 
-    requestDelay: 400  
-};
+            html += '<img src="'+mediaLocation+data[0].Images+'" alt="blog" width="100%" height="200px">';
+            html += '<div role="main" class="ui-content">'+data[0].created_dt+'<br>'+data[0].Conten+'</div>';
 
-$('#profile-type').easyAutocomplete(vehicle);
+            $('#detail-reminder').html(html);
+        },
+        error: function( errorThrown ){
+            console.log(errorThrown);
+        }
+    });
+}
 
+function get_all_riwayat() {
+    $.ajax({
+        type: 'post',
+        url: urlOdoo + "api/notice/riwayat/",
+        contentType: 'aplication/json',
+        dataType: 'json',
+        data: JSON.stringify({
+            "key_token" : "a53c6d8a114ebf02d0fb05782534c738bb8f1c8845",
+            "user_id" : user_id
+        }),
+        success: function( data ) {
+            var html = "";
+            
+            data.forEach(element => {
+                html += '<div class="card-riwayat">';
+                html += '<div class="item">Servis</div>';
+                html += '<div class="item">Tanggal</div>';
+                html += '<div class="item">Kendaraan</div>';
+                html += '<div class="item">KM</div>';
+                html += '<div class="item">Oli</div>';
+                html += '<div class="item">Waktu</div>';
+                html += '<div class="item">Notes</div>';
+                html += '<a href="#reminder-detail" class="ui-btn btn-riwayat ui-btn-inline ui-corner-all">Detail</a>';
+                html += '</div>';
+            });
+
+            $('#riwayat-list').html(html);
+        },
+        error: function( errorThrown ){
+            console.log(errorThrown);
+        }
+    });
+}
+
+function get_detail_riwayat(id) {
+    $.ajax({
+        type: 'get',
+        url: urlOdoo + "api/get/product/riwayat"+id,
+        contentType: 'aplication/json',
+        dataType: 'json',
+        success: function( data ) {
+            var html = "";
+
+            html += '<img src="'+mediaLocation+data[0].Images+'" alt="blog" width="100%" height="200px">';
+            html += '<div role="main" class="ui-content">'+data[0].created_dt+'<br>'+data[0].Conten+'</div>';
+
+            $('#detail-riwayat').html(html);
+        },
+        error: function( errorThrown ){
+            console.log(errorThrown);
+        }
+    });
+}
 
 
 function get_all_blog() {
